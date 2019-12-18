8cbd52f87398909103274585e7e537cce1d0e457
PhillipTrouble <58476984+PhillipTrouble@users.noreply.github.com>
2019-12-18 17:12:13 +0100

    Javapire

---
+++ js/exercice/1_bonjour.js
@@ -0,0 +1,12 @@
+// EXERCICE 1-bonjour.js
+
+var prompt ('Quel est ton nom');
+
+var prompt ('Quel est ton prenom');
+
+
+alert ('Bonjour' + "" + prenom + " " + nom );
+
+prompt ('nom; Prenom');
+
+// alert('js ok');
